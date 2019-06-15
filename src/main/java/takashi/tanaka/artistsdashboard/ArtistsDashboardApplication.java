7ddbffdb6340cd002862a3d1585c84751719652d
konno-yu <yuta.konno.socie@gmail.com>
2019-06-15 19:27:17 +0900

    second commit

---
+++ src/main/java/takashi/tanaka/artistsdashboard/ArtistsDashboardApplication.java
@@ -0,0 +1,13 @@
+package takashi.tanaka.artistsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistsDashboardApplication.class, args);
+	}
+
+}
